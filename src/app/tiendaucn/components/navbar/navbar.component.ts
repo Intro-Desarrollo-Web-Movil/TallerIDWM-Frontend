2e6d70696e2c7b114c008e1f0ca0f76ae26646b5
xPrismatico <srfuentesavila@gmail.com>
2025-01-03 04:52:04 -0300

    feat: add search bar to product list

--- src/app/tiendaucn/components/navbar/navbar.component.ts
+++ src/app/tiendaucn/components/navbar/navbar.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarButtonComponent } from '../navbar-button/navbar-button.component';
 import { DropdownButtonComponent } from '../dropdown-button/dropdown-button.component';
 import { CommonModule } from '@angular/common';
 import { LogoComponent } from '../logo/logo.component';
+import { NavigationEnd, Router } from '@angular/router';
+import { SearchInputComponent } from '../search-input/search-input.component';
 
 @Component({
   selector: 'navbar',
-  imports: [CommonModule, LogoComponent, NavbarButtonComponent, DropdownButtonComponent],
+  imports: [CommonModule, SearchInputComponent ,LogoComponent, NavbarButtonComponent, DropdownButtonComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  isProductListPage: boolean = false;
 
+  constructor(private router: Router) {
+    // Subscribe to router events to check the current route
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isProductListPage = event.urlAfterRedirects.includes('/product-list');
+      }
+    });
+  }
+
+  ngOnInit(): void {}
 
 }
--- src/app/tiendaucn/pages/product/product-list/product-list.component.ts
+++ src/app/tiendaucn/pages/product/product-list/product-list.component.ts
@@ -5,10 +5,11 @@ import { DropdownButtonComponent } from '../../../components/dropdown-button/dro
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { ProductCardListComponent } from '../../../components/product-card-list/product-card-list.component';
+import { SearchInputComponent } from '../../../components/search-input/search-input.component';
 
 @Component({
   selector: 'product-list',
-  imports: [CommonModule, HttpClientModule, ProductCardListComponent, NavbarComponent,NavbarButtonComponent,DropdownButtonComponent],
+  imports: [CommonModule, HttpClientModule, SearchInputComponent, ProductCardListComponent, NavbarComponent,NavbarButtonComponent,DropdownButtonComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
