59cd57be66859e840bc0bca90a9f1e7d5052ab5b
xPrismatico <srfuentesavila@gmail.com>
2025-01-05 17:54:59 -0300

    feat: add login and register forms with validations

--- src/app/tiendaucn/components/login-form/login-form.component.ts
+++ src/app/tiendaucn/components/login-form/login-form.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login-form',
-  imports: [],
+  selector: 'login-form',
+  imports: [HttpClientModule, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit{
 
+  form!: FormGroup;
+  error: boolean = false;
+  errorMessage: string[] = [];
+
+
+  constructor(private FormBuilder: FormBuilder, private router: Router) {}
+
+
+  ngOnInit(){
+    this.createForm();
+  }
+
+  createForm(){
+    this.form = this.FormBuilder.group({
+      Email:['', [Validators.required, Validators.email]],
+      Password:['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      // Navegar a otra página (por ejemplo, página de inicio)
+      this.router.navigate(['/home']);
+    } else {
+      this.error = true;
+      this.errorMessage = ['Por favor, complete todos los campos correctamente.'];
+    }
+  }
+
+
+  get passwordInvalid() {
+    return this.form.get('Password')?.invalid && this.form.get('Password')?.touched;
+  }
+  get emailInvalid() {
+    return this.form.get('Email')?.invalid && this.form.get('Email')?.touched;
+  }
 }
--- src/app/tiendaucn/pages/auth/register/register.component.ts
+++ src/app/tiendaucn/pages/auth/register/register.component.ts
@@ -4,10 +4,11 @@ import { NavbarButtonComponent } from '../../../components/navbar-button/navbar-
 import { DropdownButtonComponent } from '../../../components/dropdown-button/dropdown-button.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { RegisterFormComponent } from '../../../components/register-form/register-form.component';
 
 @Component({
   selector: 'register',
-  imports: [CommonModule, HttpClientModule, NavbarComponent,NavbarButtonComponent,DropdownButtonComponent],
+  imports: [CommonModule, HttpClientModule, NavbarComponent, RegisterFormComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
